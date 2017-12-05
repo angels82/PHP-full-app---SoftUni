af79323fbbe1d6cdcfd3903c3c7a33ec5ea53257
Angel Stoev <angels@gbg.bg>
2017-12-05 20:51:30 +0200

    Add files via upload

---
+++ PHP full app - MVC/src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('', array('name' => $name));
+    }
+
+    /**
+     * @Route("/login", name="security_login")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function login(){
+        return $this->render("security/login.html.twig");
+    }
+}
---
+++ PHP full app - MVC/src/AppBundle/Controller/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends Controller
+{
+    /**
+     * @Route("/register")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() and $form->isValid()) {
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('security_login');
+        }
+        return $this->render('register/register.html.twig', ['form'=>$form->createView()]);
+    }
+}
