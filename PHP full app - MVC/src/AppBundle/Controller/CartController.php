d0798cfd0f4d26b1ba96d2c5e5e0998181061fc7
Angel Stoev <angels@gbg.bg>
2017-12-22 14:52:36 +0200

    Add files via upload

--- PHP full app - MVC/src/AppBundle/Controller/CartController.php
+++ PHP full app - MVC/src/AppBundle/Controller/CartController.php
@@ -50,18 +50,14 @@ class CartController extends Controller
      */
     public function showAction()
     {
-        # Get object from doctrine manager
+
         $em = $this->getDoctrine()->getManager();
-        # Get logged user then get his ['id']
         $user = $this->container->get('security.token_storage')->getToken()->getUser();
-        /** Check IF user have exist cart  **/
-        # select cart from database where user id equal to cureent logged user using [ findByUser() ]
         $user_cart = $this->getDoctrine()
             ->getRepository('AppBundle:Cart')
             ->findBy(['user' => $user]);
         if ( $user_cart )
         {
-            # Then select all user cart products to display it to user
             $user_products = $this->getDoctrine()
                 ->getRepository('AppBundle:Shipping')
                 ->findBy( array('cart' => $user_cart[0]->getId()) );
@@ -94,7 +90,6 @@ class CartController extends Controller
 
 
 
-            # pass selected products to the twig page to show them
             return $this->render('cart/show.html.twig', array(
                 'products'  => $user_products,
                 'cart_data' => $user_cart[0],
@@ -108,7 +103,6 @@ class CartController extends Controller
             ));
         }
         //return new Response(''. $user_products[0]->getProduct()->getPrice() );
-        # pass selected products to the twig page to show them
         return $this->render('cart/show.html.twig');
     }
 
@@ -119,85 +113,56 @@ class CartController extends Controller
      */
     public function addAction($productId)
     {
-        # First of all check if user logged in or not by using FOSUSERBUNDLE
-        #    authorization_checker
-        # if user logged in so add the selected product to his cart and redirect user to products page
-        # else redirect user to login page to login first or create a new account
+
         $securityContext = $this->container->get('security.authorization_checker');
-        # If user logged in
         if ( $securityContext->isGranted('IS_AUTHENTICATED_FULLY') )
         {
-            # Get object from doctrine manager
             $em = $this->getDoctrine()->getManager();
-            # Get logged user then get his ['id']
             $user = $this->container->get('security.token_storage')->getToken()->getUser();
-            # for any case wewill need to select product so select it first
-            # select specific product which have passed id using ['find(passedID)']
             $product = $this->getDoctrine()
                 ->getRepository('AppBundle:Product')
                 ->find($productId);
-            /** Check IF user have exist cart  **/
-            # select cart from database where user id equal to cureent logged user using [ findByUser() ]
             $exsit_cart = $this->getDoctrine()
                 ->getRepository('AppBundle:Cart')
                 ->findBy(['user' => $user]);
-            # if there's no cart to this user create a new one
             if ( !$exsit_cart )
             {
-                # defince cart object
                 $cart = new Cart();
-                # set user whose own this cart
                 $cart->setUser($user);
 
-                # set initail total price for cart which equal to product price
                 $cart->setTotalPrice($product->getPrice());
                 $cart->setQuantity(1);
-                # persist all cart data to can use it in create shipping object
                 $em->persist($cart);
 
 
-                # flush it
                 $em->flush();
-                # create shipping object
                 $ship = new Shipping();
-                # set all its data quantity initail equal to 1 and passed product and cart created
                 $ship->setQuantity(1);
                 $ship->setProduct($product);
                 $ship->setCart($cart);
-                # persist it and flush doctrine to save it
                 $em->persist($ship);
                 $em->flush();
             }
-            # if user have one so just add new item price to cart price and add it to shipping
             else
             {
-                # Get cart from retrived object
                 $cart = $exsit_cart[0];
 
 
-                # set initail total price for cart which equal to product price
                 $cart->setTotalPrice($cart->getTotalPrice() + $product->getPrice());
-                # persist all cart data to can use it in create shipping object
                 $em->persist($cart);
-                # flush it
                 $em->flush();
-                # create shipping object
                 $ship = new Shipping();
-                # set all its data quantity initail equal to 1 and passed product and cart created
                 $ship->setQuantity(1);
                 $ship->setProduct($product);
                 $ship->setCart($cart);
-                # persist it and flush doctrine to save it
                 $em->persist($ship);
                 $em->flush();
             }
             //return new Response('user id  '.$product->getId());
             return $this->redirect($this->generateUrl('products_index'));
         }
-        # if user not logged in yet
         else
         {
-            # go to adding product form
             return $this->redirect($this->generateUrl('login'));
         }
     }
@@ -210,17 +175,12 @@ class CartController extends Controller
      */
     public function removeActione($itemProduct, $itemCart)
     {
-        # get an object from doctrine db and get Shipping Entity to work on it
         $em = $this->getDoctrine()->getManager();
         $repository = $em->getRepository('AppBundle:Shipping');
-        # select wanted item from shipping table to delete it
         $ship = $repository->findOneBy(array('product' => $itemProduct, 'cart' => $itemCart));
         //echo '<pre>'; var_dump($ship);die();
-        # Calculate the new total price for cart by subtract deleted item price from total one
         //$final_price = $ship->getCart()->getTotalPrice() - ($ship->getProduct()->getPrice() * $ship->getQuantity());
-        # update the total price of cart
         //$ship->getCart()->setTotalPrice($final_price);
-        # Remove item from db
         if ($ship) $em->remove($ship);
         $em->flush();
         return $this->redirect($this->generateUrl('view_cart'));
@@ -230,37 +190,24 @@ class CartController extends Controller
      */
     public function editActione(Request $request, $itemProduct, $itemCart)
     {
-        # in the start check if user edit field and click on button
         if ( $request->getMethod() === 'POST' )
         {
-            # read data from quantity field
             $new_quantity =$request->request->get('quantity');
-            # get oject from doctrine manager to mange operation
             $em = $this->getDoctrine()->getManager();
             $repository = $em->getRepository('AppBundle:Shipping');
-            # select wanted item from shipping table to edit it
             $ship = $repository->findOneBy(array('product' => $itemProduct, 'cart' => $itemCart));
-            # check if new quantity less than old one so subtract total price
-            # otherwise, add to it
             if( $ship->getQuantity() < $new_quantity )
             {
-                # edit selected item quantity
                 $ship->setQuantity($new_quantity);
-                # Calculate the new total price for cart by sum added item price to total one
                 $final_price = $ship->getCart()->getTotalPrice() + $ship->getProduct()->getPrice();
-                # update the total price of cart
                 $ship->getCart()->setTotalPrice($final_price);
             }
             elseif( $ship->getQuantity() > $new_quantity )
             {
-                # edit selected item quantity
                 $ship->setQuantity($new_quantity);
-                # Calculate the new total price for cart by sum added item price to total one
                 $final_price = $ship->getCart()->getTotalPrice() - $ship->getProduct()->getPrice();
-                # update the total price of cart
                 $ship->getCart()->setTotalPrice($final_price);
             }
-            # flush operations to update database
             $em->flush();
         }
         //return new Response(''. $new_quantity );
@@ -271,15 +218,11 @@ class CartController extends Controller
      */
     public function clearActione($cart)
     {
-        # get an object from doctrine db and get Shipping Entity to work on it
         $em = $this->getDoctrine()->getManager();
         $repository = $em->getRepository('AppBundle:Shipping');
-        # select wanted item from shipping table to delete it
         $ship = $repository->findBy(array('cart' => $cart));
-        # Fetch all them using foeach loop and delete them
         foreach ($ship as $one_prod)
         {
-            # Remove item from db
             $em->remove($one_prod);
             $em->flush();
         }
@@ -309,7 +252,6 @@ class CartController extends Controller
             ->getRepository('AppBundle:Cart')
             ->findBy(['user' => $user]);
 
-            # Then select all user cart products to display it to user
             $userProducts = $this->getDoctrine()
                 ->getRepository('AppBundle:Shipping')
                 ->findBy( array('cart' => $userCart[0]->getId()) );
